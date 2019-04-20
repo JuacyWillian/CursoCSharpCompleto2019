b85bca98aed10c9ee0c942311e80de72ff127c6e
Juacy Willian <juacy.willian@gmail.com>
2019-04-20 00:35:55 -0300

    exercicio 101

---
+++ Exercicio101/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// Faça um programa para ler dois valores inteiros, e depois mostrar na tela a 
+// soma desses números com uma mensagem explicativa, conforme exemplos.
+
+namespace Exercicio101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int soma = a + b;
+
+            Console.WriteLine($"SOMA = {soma}");
+        }
+    }
+}
