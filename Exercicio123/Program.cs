58a4187b756c2bbcb2c4a13c19bca995fcbd656d
Juacy Willian <juacy.willian@gmail.com>
2019-04-20 00:38:50 -0300

    exercicio 123

---
+++ Exercicio123/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// Ler um número inteiro N e calcular todos os seus divisores.
+
+namespace Exercicio123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
