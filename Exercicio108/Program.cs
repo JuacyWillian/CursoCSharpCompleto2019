401d6a4c6d8946709788f6a9a4fb19bbc3c9c313
Juacy Willian <juacy.willian@gmail.com>
2019-04-20 00:37:23 -0300

    exercicio 108

---
+++ Exercicio108/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+
+namespace Exercicio108
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0) Console.WriteLine("PAR");
+            else Console.WriteLine("IMPAR");
+
+        }
+    }
+}
