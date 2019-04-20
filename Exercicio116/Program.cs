56e9d51dc2ac612178fb1015f3359a5cb62b75fb
Juacy Willian <juacy.willian@gmail.com>
2019-04-20 00:38:11 -0300

    exercicio 116

---
+++ Exercicio116/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+// Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de 
+// pontos no sistema cartesiano. Para cada ponto escrever o quadrante a que ele pertence. 
+// O algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA 
+// (nesta situação sem escrever mensagem alguma).
+
+namespace Exercicio116
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                int x = int.Parse(vet[0]);
+                int y = int.Parse(vet[1]);
+
+                if (x == 0 || y == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    if (x > 0 && y > 0)
+                    {
+                        Console.WriteLine("primeiro");
+                    }
+                    else if (x < 0 && y > 0)
+                    {
+                        Console.WriteLine("segundo");
+                    }
+                    else if (x < 0 && y < 0)
+                    {
+                        Console.WriteLine("terceiro");
+                    }
+                    else
+                    {
+                        Console.WriteLine("quarto");
+                    }
+                }
+            }
+        }
+    }
+}
