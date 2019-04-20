8b07cfd5b0f7b1580f525821b8b4366be61cd62d
Juacy Willian <juacy.willian@gmail.com>
2019-04-20 00:37:37 -0300

    exercicio 110

---
+++ Exercicio110/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// Leia a hora inicial e a hora final de um jogo. 
+// A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia 
+// e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
+
+namespace Exercicio110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            int inicio = int.Parse(vet[0]);
+            int fim = int.Parse(vet[1]);
+            int duracao;
+
+            if (inicio < fim)
+                duracao = fim - inicio;
+            else
+                duracao = (24 - inicio) + fim;
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
