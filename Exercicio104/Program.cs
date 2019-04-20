282fc33c285563edf61816942af87b10836e642e
Juacy Willian <juacy.willian@gmail.com>
2019-04-20 00:36:59 -0300

    exercicio 104

---
+++ Exercicio104/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System;
+
+// Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, 
+// o valor que recebe por hora e calcula o salário desse funcionário. 
+// A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+
+namespace Exercicio104
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numFuncionario = int.Parse(Console.ReadLine());
+            int horasTrabalhadas = int.Parse(Console.ReadLine());
+            double salarioPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salario = horasTrabalhadas * salarioPorHora;
+
+            Console.WriteLine("NUMBER = " + numFuncionario);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
